12893ff242b0e8155512653b669b11bbd01858be
emanuele5180 <macina5180>
2020-09-21 23:45:43 +0200

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,81 @@
 <footer>
 
-  <h1>io sono il footer</h1>
+
+
+  <div class="containerFooter">
+
+    <div class="column">
+      <h2>PASTIFICIO</h2>
+      <ul>
+        <li> <a href="#"> Il pastificio</a> </li>
+        <li> <a href="#"> Grano decorticato a pietra</a> </li>
+        <li> <a href="#"> Il Molise c'è</a> </li>
+        <li> <a href="#"> Filiera integrata</a> </li>
+        <li> <a href="#"> 100 anni di pasta</a> </li>
+        <li> <a href="#"> Sartoria della pasta</a> </li>
+        <li> <a href="#"> Spaghetto quadrato</a> </li>
+        <li> <a href="#"> Le persone</a> </li>
+      </ul>
+
+    </div>
+
+    <div class="column">
+      <h2>PRODOTTI</h2>
+      <ul>
+        <li> <a href="#"> Le classiche</a> </li>
+        <li> <a href="#"> Le integrali</a> </li>
+        <li> <a href="#">Le speciali </a> </li>
+        <li> <a href="#">Le biologiche </a> </li>
+        <li> <a href="#">Le gluten-free </a> </li>
+        <li> <a href="#">Le semole </a> </li>
+        <li> <a href="#">Le extra di lusso </a> </li>
+      </ul>
+
+    </div>
+    <div class="column">
+      <h2>COLLEZIONE DA CHEF</h2>
+      <ul>
+        <li> <a href="#"> Le classiche</a> </li>
+        <li> <a href="#"> Le integrali</a> </li>
+        <li> <a href="#">Le speciali </a> </li>
+        <li> <a href="#">Le biologiche </a> </li>
+        <li> <a href="#">Le gluten-free </a> </li>
+        <li> <a href="#">Le semole </a> </li>
+        <li> <a href="#">Le extra di lusso </a> </li>
+      </ul>
+
+    </div>
+  </div>
+
+  <div class="details">
+    <div class="logocontainer">
+      <img src="{{asset('img/marchio-sito-test.png')}}" alt="">
+    </div>
+    <p>Ragione sociale: La
+      Molisana S.P.A.
+      Sede legale:
+      Contrada Colle delle
+      Api, 100/A
+      86100 -
+      Campobasso (CB)
+      Pec:
+      lamolisana@pec.it
+      Tel: +39 0874 4981
+      Fax: +39 0874
+      629584
+      info@lamolisana.it
+      (per segnalazioni
+      degli utenti)
+      commerciale@lamolisana.it
+      export@lamolisana.it
+      numero verde
+      800818081
+      telefono
+      3801292455
+    </p>
+  </div>
+
+
 
 
 </footer>
