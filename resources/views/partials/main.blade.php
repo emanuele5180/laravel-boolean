eedb3f45153bba8e8fc6918ee2e02a72b7d3cc2d
emanuele5180 <macina5180>
2020-09-22 21:53:58 +0200

    img settings

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -7,10 +7,14 @@
 
 <section class="pasta">
   <h1>LE CORTE</h1>
-  @yield('content1')
+  <div class="imgContainer">
+    @yield('content1')
+  </div>
 </section>
 
-<section class="pasta">
+<section class="pasta">  
   <h1>LE CORTISSIME</h1>
-  @yield('content2')
+  <div class="imgContainer">
+    @yield('content2')
+  </div>
 </section>
